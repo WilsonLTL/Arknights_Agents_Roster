87a9e16729989b1302ca0d788151e033ed547c48
WilsonLTL <wilsonlo1997@gmail.com>
2019-08-30 17:00:12 +0800

    Add: dist

---
+++ dist/precache-manifest.7e22915fa0049585e8e56de3d29584ca.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea863f60d14911b87b10e6ec90b49513",
+    "url": "/icons/mipmap-hdpi/ic_launcher.png"
+  },
+  {
+    "revision": "3135a777e038037c2e24080049509f68",
+    "url": "/icon.jpg"
+  },
+  {
+    "revision": "f362a4a4431aab3cb57899bb24088a8f",
+    "url": "/icons/playstore-icon.png"
+  },
+  {
+    "revision": "07bd22eda1b5c5a9f5bd",
+    "url": "/js/chunk-vendors.a0be1982.js"
+  },
+  {
+    "revision": "8370015a33852419cf3e0bb90ccb5a3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f7c949813cc5b2948326829881eb9ef",
+    "url": "/icons/mipmap-xhdpi/ic_launcher.png"
+  },
+  {
+    "revision": "9f076bf5be3a771ad471315f82e70a39",
+    "url": "/icons/mipmap-ldpi/ic_launcher.png"
+  },
+  {
+    "revision": "8a09ae8e231cef34cea0",
+    "url": "/js/app.c1a1a8a4.js"
+  },
+  {
+    "revision": "4cd4d9ab6e01f07b42f98f752d5a8421",
+    "url": "/icons/mipmap-mdpi/ic_launcher.png"
+  },
+  {
+    "revision": "90571a602f4be07c7582de6be4fd3e86",
+    "url": "/icons/mipmap-xxxhdpi/ic_launcher.png"
+  },
+  {
+    "revision": "5b3f17847a4ec0abccfea64b7396d60a",
+    "url": "/icons/mipmap-xxhdpi/ic_launcher.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "07bd22eda1b5c5a9f5bd",
+    "url": "/css/chunk-vendors.80b51937.css"
+  },
+  {
+    "revision": "8a09ae8e231cef34cea0",
+    "url": "/css/app.9cc99f8c.css"
+  }
+];
